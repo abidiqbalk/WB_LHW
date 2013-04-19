cfc0164e9b5f30a5f94c64a1b1b783c4211ec8f6
abid25 <abidiqbal8@yahoo.com>
2013-04-19 09:26:59 -0400

    temporarily commented task scheduler code

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,38 +1,38 @@
-require 'rubygems'
-require 'rufus/scheduler'  
-scheduler = Rufus::Scheduler.start_new
+# require 'rubygems'
+# require 'rufus/scheduler'  
+# scheduler = Rufus::Scheduler.start_new
 
-fp_lock = 1 # simple semaphore construct to synchronize import calls. Only two threads will ever be in play here. Check and update of lock should atomic but 
-# in this case i don't think it's needed. I hope...
+# fp_lock = 1 # simple semaphore construct to synchronize import calls. Only two threads will ever be in play here. Check and update of lock should atomic but 
+# # in this case i don't think it's needed. I hope...
 
-scheduler.every("300s") do
-	begin
-	if fp_lock == 1
-		fp_lock = 0 # get the lock
+# scheduler.every("300s") do
+# 	begin
+# 	if fp_lock == 1
+# 		fp_lock = 0 # get the lock
 
-		ChildHealth.import_data
-		HealthHouse.import_data
-		Maternal.import_data 
-		Newborn.import_data 
-		SpecialTask.import_data
-		SupportGroupMeeting.import_data
-	 	FpClient.import_data
+# 		ChildHealth.import_data
+# 		HealthHouse.import_data
+# 		Maternal.import_data 
+# 		Newborn.import_data 
+# 		SpecialTask.import_data
+# 		SupportGroupMeeting.import_data
+# 	 	FpClient.import_data
 		
-		ReportingCommunityMeeting.import_data
-		ReportingFacility.import_data
-		ReportingBirthDeath.import_data
- 		ReportingChildHealth.import_data
-		ReportingFamilyPlanning.import_data 
-		ReportingMaternalHealth.import_data
-        ReportingTreatment.import_data
+# 		ReportingCommunityMeeting.import_data
+# 		ReportingFacility.import_data
+# 		ReportingBirthDeath.import_data
+#  		ReportingChildHealth.import_data
+# 		ReportingFamilyPlanning.import_data 
+# 		ReportingMaternalHealth.import_data
+#         ReportingTreatment.import_data
 		
-		fp_lock = 1 #release the lock
-	else
-		puts "Skipping import. Previous cycle not yet complete."
-	end
-	rescue Exception => exc
-		fp_lock = 1
-		puts "Message for the log file #{exc.message}"
-		puts exc.backtrace.join("\n")
-	end
-end
+# 		fp_lock = 1 #release the lock
+# 	else
+# 		puts "Skipping import. Previous cycle not yet complete."
+# 	end
+# 	rescue Exception => exc
+# 		fp_lock = 1
+# 		puts "Message for the log file #{exc.message}"
+# 		puts exc.backtrace.join("\n")
+# 	end
+# end
